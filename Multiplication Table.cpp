ca589341297e66ae278e3e34c7439513cb78ee76
suhas0407 <118986814+suhas0407@users.noreply.github.com>
2023-02-24 09:16:04 +0530

    Add files via upload

---
+++ Multiplication Table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int i,n;
+
+    cout<<"Enter n";
+    cin>>n;
+
+    for(i=1;i<=10;i++)
+    {
+        cout<<n<<"x"<<i<<"="<<i*n<<endl;
+    }
+}
